2433c09d42b88a52eed5693bce9e299d20c2a03e
andreaconv <andrea2convertini@gmail.com>
2023-04-14 12:40:15 +0200

    [add] assets

---
+++ assets/JS/script.js
@@ -0,0 +1,21 @@
+const {createApp} = Vue;
+
+createApp({
+
+  data(){
+
+    return {
+      msg:'HELLO',
+    }
+
+  },
+
+  methods:{
+    //FUNZIONI
+  },
+
+  mounted() {
+    console.log("vue funziona")
+
+  }
+}).mount('#app')
